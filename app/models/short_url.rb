93cd072a98a4c1f102af77c1707e6a33bb3d5e7d
vitthal.gaikwad <gaikwadvitthal04@gmail.com>
2017-09-17 06:06:02 -0700

    Add gem base62-rb to create short url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,12 +1,14 @@
+require 'base62-rb'
+
 class ShortUrl < ActiveRecord::Base
 
   def short_url
-    # Create short url using id and salt and converting this into base36 number
-    "#{ Rails.env.production? ? PROD_HOST_URL : DEV_HOST_URL }/#{ (id.to_s + salt.to_s).reverse.to_i.to_s(36) }"
+    # Create short url using id and salt and converting this into base62 number
+    "#{ Rails.env.production? ? PROD_HOST_URL : DEV_HOST_URL }/#{ Base62.encode((id.to_s + salt.to_s).reverse.to_i) }"
   end
 
   def self.short_url_to_id(url)
-    # Convert short url into base36 number and reverse it, then remove last number which is nothing but salt
-    url.to_i(36).to_s.reverse.chop.to_i
+    # Convert short url into base62 number and reverse it, then remove last number which is nothing but salt
+    Base62.decode(url).to_s.reverse.chop.to_i
   end
 end
